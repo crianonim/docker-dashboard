8a84f1b0f4201f38f9babaee8129b0dcfcba232b
Jan Skowronski <crianonim@gmail.com>
2019-08-29 10:31:44 +0100

    Start Material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
-
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton></IconButton>
+        </Toolbar>
+      </AppBar>
       <Button variant="contained" color="primary">Click me</Button>
+      <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+      </Box>
+    </Container>
     </div>
   );
 }
